edba3a5c0461c189c1c8150e3b2ce1ceee1536e2
gstan <gstan@P5RO-PORT727>
2017-08-16 10:30:41 +0300

    Add a ViewModel

---
+++ VidlyGeoApp/ViewModels/RandomMovieViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using VidlyGeoApp.Models;
+
+namespace VidlyGeoApp.ViewModels
+{
+    public class RandomMovieViewModel
+    {
+        public Movie Movie { get; set; }
+        public List<Customer> Customers  { get; set; }        
+    }
+}
