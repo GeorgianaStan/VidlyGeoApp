506365eb24623a0af6db8ac2148aa6984cf513c7
gstan <gstan@P5RO-PORT727>
2017-08-16 11:25:34 +0300

    Diplay Customers

---
+++ VidlyGeoApp/Controllers/CustomersController.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using VidlyGeoApp.Repository;
+
+namespace VidlyGeoApp.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        public ViewResult Index()
+        {
+            var customers = CustomersRepository.GetCustomers();
+            return View(customers);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var customer = CustomersRepository.GetCustomers().SingleOrDefault(c => c.Id == id);
+            if (customer == null)
+                return HttpNotFound();
+
+            return View(customer);
+        }
+    }
+}
---
+++ VidlyGeoApp/Repository/CustomersRepository.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using VidlyGeoApp.Models;
+
+namespace VidlyGeoApp.Repository
+{
+    public static class CustomersRepository
+    {
+        public static IEnumerable<Customer> GetCustomers()
+        {
+            return new List<Customer>()
+            {
+                new Customer() { Id = 1, Name = "John Smith" },
+                new Customer() { Id = 2, Name = "mary Williams" }
+            };
+        }
+    }
+}
