c51544e5d29f0d149ab6c09166a90e1a4eb49d7b
gstan <gstan@P5RO-PORT727>
2017-08-16 11:24:37 +0300

    Removed unused usings

--- VidlyGeoApp/Models/Movie.cs
+++ VidlyGeoApp/Models/Movie.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace VidlyGeoApp.Models
+﻿namespace VidlyGeoApp.Models
 {
     public class Movie
     {
