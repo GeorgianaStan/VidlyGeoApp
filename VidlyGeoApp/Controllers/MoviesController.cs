35ee698ac78b0cadbdb937c0eb3bba95e17cd87a
gstan <gstan@P5RO-PORT727>
2017-08-16 11:26:06 +0300

    Display Movies

---
+++ VidlyGeoApp/Controllers/MoviesController.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Mvc;
+using VidlyGeoApp.Repository;
+
+namespace VidlyGeoApp.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ViewResult Index()
+        {
+            var movies = MoviesRepository.GetMovies();
+            return View(movies);
+        }
+    }
+}
---
+++ VidlyGeoApp/Repository/MoviesRepository.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using VidlyGeoApp.Models;
+
+namespace VidlyGeoApp.Repository
+{
+    public static class MoviesRepository
+    {
+        public static IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>()
+            {
+                new Movie() { Id = 1, Name = "Shrek" },
+                new Movie() { Id = 1, Name = "Wall-e" }
+            };
+        }
+    }
+}
