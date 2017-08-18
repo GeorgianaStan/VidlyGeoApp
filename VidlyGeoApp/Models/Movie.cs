c9c5d00fe31469d266eac319158a3ce1e6bb472f
gstan <gstan@P5RO-PORT727>
2017-08-18 12:42:55 +0300

    Update Ex3 Display the genres

--- VidlyGeoApp/Models/Movie.cs
+++ VidlyGeoApp/Models/Movie.cs
@@ -6,14 +6,22 @@ namespace VidlyGeoApp.Models
     public class Movie
     {
         public int Id { get; set; }
+          
         [Required]
         public string Name { get; set; }
+
         [Required]
-        public string Genre { get; set; }
+        public Genre Genre { get; set; }
+
+        [Required]
+        public byte GenreId { get; set; }
+
         [Required]
         public DateTime ReleaseDate { get; set; }
+
         [Required]
         public DateTime DateAdded { get; set; }
+
         [Required]
         public int NumberInStock { get; set; } 
 
